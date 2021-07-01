5de282922c7002bf720ffb069cb9a11be4154b13
abdur-razzak95 <razzak.bu95@gmail.com>
2021-07-01 05:34:53 -0700

    boshudha 3

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//$(document).ready(function(){
+//  $(".owl-carousel").owlCarousel();
+//});
+//
+////$(document).ready(function() {
+//// 
+////  $("#owl-demo").owlCarousel({
+////    navigation : true
+////  });
+//// 
+////});
+
